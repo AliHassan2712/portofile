fec07861a08dc85adb2ff9a8c293ecc265bdad3b
AliHassan2712 <your-email@example.com>
2025-08-02 14:18:47 +0300

    Add initial project files including SVG icons, images, JavaScript functionality, and HTML structure for portfolio website

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+};
+
+for (let i = 0; i < revealDelayElements.length; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+const progressFills = document.querySelectorAll('.progress-fill');
+
+function animateProgress() {
+  const triggerPoint = window.innerHeight;
+
+  progressFills.forEach(fill => {
+    const rect = fill.getBoundingClientRect();
+    if (rect.top <= triggerPoint) {
+      fill.style.width = fill.dataset.prog;
+    } else {
+      fill.style.width = '0';
+    }
+  });
+}
+
+window.addEventListener('scroll', animateProgress);
+window.addEventListener('load', animateProgress);
